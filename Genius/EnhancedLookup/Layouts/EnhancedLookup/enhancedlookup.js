b73ce254873c16975f625bebc1e74bb4b817f70e
Eirik Brandtzaeg <eirikb@eirikb.no>
2012-07-20 23:29:41 +0200

    Initial commit of the enhanced lookup

---
+++ Genius/EnhancedLookup/Layouts/EnhancedLookup/enhancedlookup.js
@@ -0,0 +1,3 @@
+﻿Array.prototype.slice.call(document.querySelectorAll('.ms-formtable .ms-formbody')).filter(function (el) { return el.innerHTML.match(/FieldType=\"SPFieldLookupMulti\"/); }).forEach(function (multi) {
+    console.log(multi)
+});
